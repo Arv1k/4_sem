27de27609d1b3b7a72b5b56e5ccdc5e4ae17068a
Arv1k <you@example.com>
2020-05-21 00:39:26 +0300

    In process.

--- controlnaya/2/main.cpp
+++ controlnaya/2/main.cpp
@@ -8,6 +8,14 @@ std::mutex lock;
 
 const int max_val = 512;
 
+#define DEBUG
+
+#ifdef DEBUG
+#define PRINTF printf
+#else
+#define PRINTF ;
+#endif
+
 int main(int argc, char** argv) {
     FILE* file_1 = fopen(argv[1], "rb");
     FILE* file_2 = stdout;
@@ -18,40 +26,51 @@ int main(int argc, char** argv) {
     buffer buf = buffer(1024);
 
     std::thread thread_read([](buffer& buf, FILE* file_read, std::promise<int>& pr_read) {
+        buf.change_flag(1);
+
+        int flag = 0;
         int ch_read = 0;
         int cur_offset = 0;
-        buf.change_flag(1);
-        int flag = 0;
-
         do {
             if (flag == 1) {
+                PRINTF("\nReader: Can I write in buffer?\n\n");
+
                 lock.lock();
-                if (buf.size() < 0.2*buf.capacity()) {
+                if (buf.size() > 0.2*buf.capacity()) {
+                    PRINTF("\nReader: No, I can't.\n\n");
+
+                    lock.unlock();
                     continue;
                 }
+
+                PRINTF("\nReader: Yes, configuring myself!\n\n");
                 cur_offset = buf.size();
                 lock.unlock();
+
                 flag = 0;
             } else if (cur_offset >= buf.capacity()) {
+                PRINTF("\nReader: Buffer is full. Waiting.\n\n");
+
                 cur_offset = 0;
                 flag = 1;
 
                 continue;
             }
 
+            lock.lock();
             ch_read = fread(buf.buf() + cur_offset, 1, max_val, file_read);
             buf.plus_size(ch_read);
+            lock.unlock();
+
             cur_offset += ch_read;
 
-            //printf("\nEto ya read %d\n", ch_read);
-            //printf("buf size: %d, my offset: %d\n\n", buf.size(), cur_offset);
+            PRINTF("\nReader: read %d\n", ch_read);
+            PRINTF("Reader: buf size: %d, my offset: %d\n\n", buf.size(), cur_offset);
 
-            //std::this_thread::sleep_for(std::chrono::seconds(1));
-            //lock.unlock();
+            std::this_thread::sleep_for(std::chrono::seconds(1));
         } while (ch_read != 0);
 
         buf.change_flag(0);
-        // pr_read.set_value(100);
     }, std::ref(buf), file_1, std::ref(pr_read));
 
     std::thread thread_write([](buffer& buf, FILE *file_write, std::future<int>& fut_write) {
@@ -59,37 +78,47 @@ int main(int argc, char** argv) {
 
         int ch_write = 0;
         int cur_offset = 0;
-        int flag = 0;
+        int flag = 1;
         do {
-            //lock.lock();
             if (flag == 1) {
-                lock.unlock();
+                PRINTF("\nWriter: Can I read from buffer?\n\n");
+
+                lock.lock();
                 if (buf.size() < 0.8*buf.capacity()) {
+                    PRINTF("\nWriter: No, I can't.\n\n");
+
+                    lock.unlock();
                     continue;
                 }
+
+                PRINTF("\nWriter: Yes, Let's go!.\n\n");
                 lock.unlock();
+
                 flag = 0;
             } else if (cur_offset >= buf.capacity()) {
+                PRINTF("\nWriter: Buffer is empty. Waiting.\n\n");
+
                 cur_offset = 0;
                 flag = 1;
 
                 continue;
             }
 
+            lock.lock();
             int to_write = (buf.size() >= max_val)? max_val : buf.size();
             ch_write = fwrite(buf.buf() + cur_offset, 1, to_write, file_write);
             buf.minus_size(ch_write);
+            lock.unlock();
+
             cur_offset += ch_write;
 
-            //printf("\n\nEto ya write %d\n", ch_write);
-            //printf("buf size: %d, my offset: %d\n\n", buf.size(), cur_offset);
+            PRINTF("\nWriter: wrote %d\n", ch_write);
+            PRINTF("Writer: buf size: %d, my offset: %d\n\n", buf.size(), cur_offset);
 
-            //std::this_thread::sleep_for(std::chrono::seconds(1));
-            //lock.unlock();
+            std::this_thread::sleep_for(std::chrono::seconds(1));
         } while (buf.size() != 0 || buf.flag() == 1);
     }, std::ref(buf), file_2, std::ref(fut_write));
 
-    //printf("A ya jdu\n");
     thread_read.join();
     thread_write.join();
 }
