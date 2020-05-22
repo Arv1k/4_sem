b522e2790a47c9c93ba3c253cb3f5973c9f89222
Arv1k <you@example.com>
2020-05-23 00:10:01 +0300

    In process x2.

--- controlnaya/2/buffer.h
+++ controlnaya/2/buffer.h
@@ -8,6 +8,9 @@ private:
     int _capacity;
     int _size;
     int _flag;
+    std::pair <std::promise<void>, std::future<void>> p_read_;
+    std::pair <std::promise<void>, std::future<void>> p_write_;
+    std::mutex lock;
 
 public:
     explicit buffer(int cap) {
@@ -15,6 +18,9 @@ public:
         _capacity = cap;
         _size = 0;
         _flag = 0;
+
+        p_read_.second  = p_write_.first.get_future();
+        p_write_.second = p_read_.first.get_future();
     };
 
     ~buffer() {
@@ -37,11 +43,15 @@ public:
     }
 
     void plus_size(int heh) {
+        lock.lock();
         _size += heh;
+        lock.unlock();
     }
 
     void minus_size(int heh) {
+        lock.lock();
         _size -= heh;
+        lock.unlock();
     }
 
     int flag() {
@@ -50,6 +60,28 @@ public:
 
     void change_flag(int new_flag) {
         _flag = new_flag;
+    }
+
+    void read_set() {
+        p_read_.first.set_value();
+    }
+
+    void write_set() {
+        p_write_.first.set_value();
+    }
+
+    void read_get() {
+        p_read_.second.get();
+
+        p_write_.first = std::promise<void> ();
+        p_read_.second = p_write_.first.get_future();
+    }
+
+    void write_get() {
+        p_write_.second.get();
+
+        p_read_.first = std::promise<void> ();
+        p_write_.second = p_read_.first.get_future();
     }
 };
 
--- controlnaya/2/main.cpp
+++ controlnaya/2/main.cpp
@@ -4,11 +4,9 @@
 #include <future>
 #include "buffer.h"
 
-std::mutex lock;
 
-const int max_val = 1024;
+//#define DEBUG
 
-    #define DEBUG
 
 #ifdef DEBUG
 #define PRINTF printf
@@ -16,27 +14,20 @@ const int max_val = 1024;
 #define PRINTF ;
 #endif
 
+const int max_val = 512;
+
 int main(int argc, char** argv) {
     FILE* file_1 = fopen(argv[1], "rb");
     FILE* file_2 = stdout;
 
-    std::promise<int> pr_read;
-    std::promise<int> pr_write;
+    buffer buf(1024);
 
-    std::future<int> fut_read = pr_write.get_future();
-    std::future<int> fut_write = pr_read.get_future();
-
-    buffer buf = buffer(1096);
-
-    std::thread thread_read([](buffer& buf, FILE* file_read,
-                               std::promise<int>& pr_read,
-                               std::future<int>& fut_write) {
+    std::thread thread_read([](buffer& buf, FILE* file_read) {
         buf.change_flag(1);
 
         int ch_read = 0;
         int cur_offset = 0;
         do {
-            lock.lock();
             ch_read = fread(buf.buf() + cur_offset, 1, max_val, file_read);
             buf.plus_size(ch_read);
             cur_offset += ch_read;
@@ -44,55 +35,43 @@ int main(int argc, char** argv) {
             PRINTF("\nReader: read %d\n", ch_read);
             PRINTF("Reader: buf size: %d, my offset: %d\n\n", buf.size(), cur_offset);
 
-            if (buf.size() > 0.8*buf.capacity()) {
-                pr_read.set_exception(0);
-                pr_read.set_exception(0);
+            if (buf.size()*100 > 80*buf.capacity()) {
+                buf.read_set();
             }
-            lock.unlock();
 
             if (cur_offset >= buf.capacity()) {
-                fut_write.wait();
+                buf.read_get();
 
                 cur_offset = 0;
             }
 
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            //std::this_thread::sleep_for(std::chrono::seconds(1));
         } while (ch_read != 0);
 
         buf.change_flag(0);
-    }, std::ref(buf), file_1, std::ref(pr_read), std::ref(fut_write));
+    }, std::ref(buf), file_1);
 
-    std::thread thread_write([](buffer& buf, FILE *file_write,
-                                std::promise<int>& pr_write,
-                                std::future<int>& fut_read) {
-        //std::this_thread::sleep_for(std::chrono::seconds(1));
-
+    std::thread thread_write([](buffer& buf, FILE *file_write) {
         int ch_write = 0;
         int cur_offset = 0;
-        int flag = 1;
         do {
-            /*lock.lock();
             if (buf.size() == 0) {
-                lock.unlock();
-
-                fut_read.valid();
+                buf.write_get();
 
                 cur_offset = 0;
-            }*/
+            }
 
-            lock.lock();
             int to_write = (buf.size() >= max_val)? max_val : buf.size();
             ch_write = fwrite(buf.buf() + cur_offset, 1, to_write, file_write);
             buf.minus_size(ch_write);
             cur_offset += ch_write;
 
-            if (buf.size() < 0.2*buf.capacity()) {
-                pr_write.set_exception(0);
+            if (buf.size()*100 < 2*buf.capacity()) {
+                buf.write_set();
             }
-            lock.unlock();
 
             if (cur_offset >= buf.capacity()) {
-                fut_read.wait();
+                buf.write_get();
 
                 cur_offset = 0;
             }
@@ -100,9 +79,9 @@ int main(int argc, char** argv) {
             PRINTF("\nWriter: wrote %d\n", ch_write);
             PRINTF("Writer: buf size: %d, my offset: %d\n\n", buf.size(), cur_offset);
 
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            //std::this_thread::sleep_for(std::chrono::seconds(1));
         } while (buf.size() != 0 || buf.flag() == 1);
-    }, std::ref(buf), file_2, std::ref(pr_write), std::ref(fut_read));
+    }, std::ref(buf), file_2);
 
     thread_read.join();
     thread_write.join();
